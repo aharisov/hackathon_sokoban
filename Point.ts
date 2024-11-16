6729e71ec472859a9ccd728278169998baa817e6
Aleksandr Harisov <aharisov@smbindustries.fr>
2024-11-16 10:31:47 +0100

    code refactor in Point class

--- Point.ts
+++ Point.ts
@@ -1,5 +1,5 @@
-import { Direction } from "./Direction";
-import { Shape } from "./Shape";
+import { Direction } from "./Direction.js";
+import { Shape } from "./Shape.js";
 
 export class Point {
     protected x: number;
@@ -44,49 +44,35 @@ export class Point {
         return this.x == otherPoint.getX() && this.y == otherPoint.getY();
     }
 
-    public collide(width: number, height: number): boolean {
-        console.log('collide', this.x, this.y);
-        
-        if( this.x < 0 || this.x >= width || this.y < 0 || this.y >= height ) return true;
+    public isWall(width: number, height: number): boolean {
+        if( this.x < 0 || this.x > width || this.y < 0 || this.y > height ) return true;
         
         return false;
     }
 
     public move(dir: Direction, w: number, h: number): void {
-        let isCollide: boolean = this.collide(w, h);
+        let xTmp: number = this.x;
+        let yTmp: number = this.y;
         
         switch (dir) {
-            case 0:
-                if (isCollide) {
-                    this.y >= h ? this.y-- : this.y = 0;
-                } else {
-                    this.y <= 0 ? this.y = 0 : this.y--;
-                }
+            case Direction.UP:
+                this.y--;
                 break;
-            case 1:
-                if (isCollide) {
-                    this.y <= 0 ? this.y++ : this.y = h;
-                } else {
-                    this.y++;
-                }
+            case Direction.DOWN:
+                this.y++;
                 break;
-            case 2:
-                if (isCollide) {
-                    this.x >= w ? this.x-- : this.x = 0;
-                } else {
-                    this.x <= 0 ? this.x = 0 : this.x--;
-                }
+            case Direction.LEFT:
+                this.x--;
                 break;
-            case 3:
-                if (isCollide) {
-                    this.x <= 0 ? this.x++ : this.x = w;
-                } else {
-                    this.x++;
-                }
+            case Direction.RIGHT:
+                this.x++;
                 break;
         }
 
-        console.log(this.x, this.y, isCollide);
+        if (this.isWall(w, h)) {
+            this.x = xTmp;
+            this.y = yTmp;
+        }
         
     }
 }
